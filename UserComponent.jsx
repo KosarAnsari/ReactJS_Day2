8ff008a0c34846f4ac941b97ad0ed4ed0f865ea4
KosarAnsari <kosaransari55@gmail.com>
2025-07-18 20:30:45 +0530

    Create UserComponent.jsx

---
+++ UserComponent.jsx
@@ -0,0 +1,31 @@
+function Login() {
+  return (
+    <>
+      <h1>Login User</h1>
+    </>
+  );
+}
+
+export function Profile() {
+  return (
+    <>
+      <h2>Profile</h2>
+    </>
+  );
+}
+
+export function Setting() {
+  return (
+    <>
+      <h2>Settings</h2>
+    </>
+  );
+}
+
+export const UserKey = '@kosarAnsari#1705';
+
+export default Login;
+
+// A component can have only single default export
+// but can have multiple named exports like profile and setting in this file.
+// you can also import and export variables
