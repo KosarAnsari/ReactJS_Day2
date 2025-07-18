c6b7a5f09d064b1d87b183ec42b3abe4038f27d7
KosarAnsari <kosaransari55@gmail.com>
2025-07-18 20:28:21 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,29 @@
+import Login from './UserComponent';
+import { Profile, Setting, UserKey } from './UserComponent';
+import Exercise from './ExerciseComp';
+
+function App() {
+  const UserName = 'Achlys';
+  let x = 10;
+  let y = 2;
+
+  return (
+    <>
+      <h1>importing and exporting Components.</h1>
+      <Login />
+      <Profile />
+      <Setting />
+      <h2>{UserKey}</h2> // Way of exporting a variable
+      <h2>{UserName}</h2>
+      <h3>{x + y}</h3>
+      <p>{10 + 20 + 30}</p>
+      <button onClick={() => alert('Welcome to ReactJS Day 2')}>
+        Click here!
+      </button>
+      <Exercise />
+    </>
+  );
+}
+
+export default App;
 
